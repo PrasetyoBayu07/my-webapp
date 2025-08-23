2ef87c7ef490f46dc7ee0ec4f4943794fb48ac2c
PrasetyoBayu07 <102822180+PrasetyoBayu07@users.noreply.github.com>
2025-08-24 03:23:47 +0700

    Delete src directory

--- src/pages/maintenance.js
+++
@@ -1,8 +0,0 @@
-export default function Maintenance() {
-  return (
-    <div className="maintenance">
-      <h2>🚧 Sedang Maintenance</h2>
-      <p>Aplikasi sedang dalam perbaikan, coba lagi nanti.</p>
-    </div>
-  );
-}
